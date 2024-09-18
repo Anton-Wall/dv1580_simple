97bf2e945d217c3476d6f6eb06050b74ea3c9d90
Anton-Wall <112175050+Anton-Wall@users.noreply.github.com>
2024-09-18 10:08:48 +0200

    Update mult.c

--- mult.c
+++ mult.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    printf("%i", argv[1]*argv[2];
-  return 0;
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int product = num1 * num2;
+  return product;
 };
