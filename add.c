6004bddf1167c85847550d6ce7677997d2440a8f
Anton-Wall <112175050+Anton-Wall@users.noreply.github.com>
2024-09-18 08:57:12 +0200

    Update add.c

--- add.c
+++ add.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-
+    int argv1;
+    int argv2;
+    scanf("%d", &argv1);
+    scanf("%d", &argv2);
+    printf("\n%i", argv1 + argv2);
   return 0;
 }
