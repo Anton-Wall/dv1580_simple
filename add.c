62d18770fa8527fd6d18e8bb37b8fb888a9d5e00
Anton-Wall <112175050+Anton-Wall@users.noreply.github.com>
2024-09-18 09:39:08 +0200

    Update add.c

--- add.c
+++ add.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    int num1 = atoi(argv[1])
-    int num2 = atoi(argv[2])
-    printf("%i", mum1 + num2);
-    return 0;
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int sum = num1 + num2;
+    return sum;
 };
