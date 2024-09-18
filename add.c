59ec2dc5fee7e8e8075f4ae39b335ada8fad18a5
Anton-Wall <112175050+Anton-Wall@users.noreply.github.com>
2024-09-18 09:02:34 +0200

    Update add.c

--- add.c
+++ add.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    int argv1;
-    int argv2;
-    scanf("%d", &argv1);
-    scanf("%d", &argv2);
-    printf("\n%i", argv1 + argv2);
+    printf("%i", argv[1]+ argv[2]);
   return 0;
 };
