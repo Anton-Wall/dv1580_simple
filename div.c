feeb014fe8c13a35643f7873546bf60d7fa290fd
Anton-Wall <112175050+Anton-Wall@users.noreply.github.com>
2024-09-18 09:20:48 +0200

    Update div.c

--- div.c
+++ div.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-
+    if (argv[2] == 0){
+      printf("DIV ZERO NOT ALLOWED!\n")
+    } else {
+      printf("%i", argv[1]/argv[2])
+    };
   return 0;
-}
+};
